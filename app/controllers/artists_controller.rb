730e0b33056d0bd9b84cb395babaca1552542ee5
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-20 21:13:21 +0100

    complete artists#create

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,4 +5,18 @@ class ArtistsController < ApplicationController
 
   def new
   end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  private 
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
