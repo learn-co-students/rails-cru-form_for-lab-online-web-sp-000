b4236cf0422af77858fe5237e9e778f22a741f19
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-20 21:15:02 +0100

    complete artists#edit

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,9 +10,15 @@ class ArtistsController < ApplicationController
     @artist = Artist.create(artist_params)
     redirect_to artist_path(@artist)
   end
-
+  
   def edit
     @artist = Artist.find(params[:id])
+  end 
+  
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
   end
 
   private 
