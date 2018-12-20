c45781e8aa9123d9483f6150ec48987cf2be572d
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-20 21:20:44 +0100

    complete songs#show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @song = Song.create(genre_params)
+    redirect_to genre_path(@song)
+  end
+  
+  def edit
+    @song = Song.find(params[:id])
+  end 
+  
+  def update
+    @song = Song.find(params[:id])
+    @song.update(genre_params)
+    redirect_to genre_path(@song)
+  end
+
+  private 
+  def genre_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
 end
