adbb8eca4e1ae16461767ae8d24a253753d583c2
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-15 10:57:46 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,14 @@
 class ArtistsController < ApplicationController
+  def show
+    @artist = Artist.find(params[:id])
+  end
+  
+  def new
+    @artist = Artist.new
+  end
+  
+  def create
+    # @artist = Artist.new(params.require(:artist))
+    redirect_to artist_path(@artist)
+  end
 end
