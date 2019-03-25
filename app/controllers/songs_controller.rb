af01b438793eba78f1a52041bce3050c16bc6de5
github username <github email address>
2019-03-24 20:36:09 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,13 +4,13 @@ class SongsController < ApplicationController
   def index
     @song = Song.new
   end
-  
+
   def new
    @song = Song.new
  end
 
  def create
-   @song = Song.new(song_params(:name))
+   @song = Song.new(song_params(:name, :artist_id, :genre_id))
    @song.save
    redirect_to song_path(@song)
  end
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
 
  def update
    @song = Song.find(params[:id])
-   @song.update(song_params(:name))
+   @song.update(song_params(:name, :artist_id, :genre_id))
    redirect_to song_path(@song)
  end
 
