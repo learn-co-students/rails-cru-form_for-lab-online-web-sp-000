21eec87bb3d3a9d2983a5e7b2bcc6eecdb1f37f1
github username <github email address>
2019-03-24 21:32:04 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,7 @@
 class ArtistsController < ApplicationController
 
   def new
-   @artists = Artist.all
+   @artist = Artist.new
  end
 
  def create
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,7 @@
 class GenresController < ApplicationController
 
   def new
-   @genres = Genre.all
+   @genre = Genre.new
  end
 
  def create
