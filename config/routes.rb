0a5db047e448d6e2eceec2c3df128df45a4080e2
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-04-24 19:52:47 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, only: %i[index show new create edit update]
+  resources :genres, only: %i[index show new create edit update]
+  resources :artists, only: %i[index show new create edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
