b726b1057e1412954e388b6bba4411b24f697dc9
Tommy Curley <thomas.curley93@gmail.com>
2019-06-11 15:34:48 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/new'
+
+  get 'songs/edit'
+
+  get 'songs/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'artists/show'
+
+  get 'genres/new'
+
+  get 'genres/edit'
+
+  get 'genres/show'
+
+  get 'genres_controller/new'
+
+  get 'genres_controller/edit'
+
+  get 'genres_controller/show'
+
+  get 'artists_controller/new'
+
+  get 'artists_controller/edit'
+
+  get 'artists_controller/show'
+
+  get 'songs_controller/new'
+
+  get 'songs_controller/edit'
+
+  get 'songs_controller/show'
+
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
