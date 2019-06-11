d2c59a46bc5b4a491f1bfd39f199fd3c610a59bb
ch2isk4kos <ch2isk4kos@gmail.com>
2019-06-11 15:14:35 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+
+    before_action :find_artist, only: [:show, :edit, :update]
+
+    def index
+        @artists = Artist.all.order("created_at DESC")
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+        redirect_to artists_path(@artist)
+    end
+
+    def show; end
+
+    def edit; end
+
+    def update
+        @artist.update(artist_params)
+        redirect_to artists_path(@artist)
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+
+    def find_artist
+        @artist = Artist.find(params[:id])
+    end
+
+end
