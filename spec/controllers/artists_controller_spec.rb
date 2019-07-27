83711b217f9d413cd8a26367cbde7e079996d8a9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-27 00:57:42 +0000

    Done.

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe SongsController, type: :controller do
+RSpec.describe ArtistsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
@@ -12,13 +12,6 @@ RSpec.describe SongsController, type: :controller do
   describe "GET #edit" do
     it "returns http success" do
       get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
   end
