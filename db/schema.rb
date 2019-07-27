1aca18180c6bfa1d8bfccb99ca68e27301678e04
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-27 04:11:43 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20190727001430) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
-    t.string   "artist_id"
+    t.integer  "artist_id"
     t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
