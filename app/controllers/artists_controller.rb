2e5da178c19f2e7d1442a7aa3382878e5cc38b5a
Ramon Chavez <rachavez1974@gmail.com>
2019-08-13 15:09:01 -0400

    Add show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,16 @@
 class ArtistsController < ApplicationController
-  
+  before_action :set_artist, only: [:show, :edit, :update]
+
+  def show
+    
+  end
+
+  private
+    def artist_params(*args)
+      params.require(:artist).permit(*args)
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end  
 end
