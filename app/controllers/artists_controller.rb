2e9d10113e2634dff9b8690dd5d47c61debce1b1
Ramon Chavez <rachavez1974@gmail.com>
2019-08-13 15:46:02 -0400

    Add new

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,21 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update]
 
+  def new
+    @artist = Artist.new
+  end
+
   def show
     
+  end
+
+  def create
+    @artist = Artist.new(artist_params(params[:artist].keys))
+      if @artist.save
+        redirect_to @artist
+      else
+        render 'new'
+      end
   end
 
   private
