bfd9708b45bbc4746f86c23bb602c02ac42f22f1
Ramon Chavez <rachavez1974@gmail.com>
2019-08-13 16:29:17 -0400

    Add showl

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,15 @@
 class GenresController < ApplicationController
+    before_action :set_genre, only: [:show, :edit, :update]
+
+  def show
+    
+  end
+
+  private
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
+    def set_genre
+      @genre = Genre.find(params[:id])
+    end
 end
