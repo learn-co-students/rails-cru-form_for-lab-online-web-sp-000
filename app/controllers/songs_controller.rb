7442e721453b05be78f3c6f9a61db5a30c3b67cd
Ramon Chavez <rachavez1974@gmail.com>
2019-08-13 17:28:24 -0400

    Add links to show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,12 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  private
+   
+    def set_song
+      @song = Song.find(params[:id])
+    end
 end
