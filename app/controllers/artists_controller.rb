9ecf38925cf08b192a1e70ead7f9aa114a0e83fb
jgrantps <jgrantps@hotmail.com>
2019-10-08 22:26:09 -0300

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,43 @@
+class ArtistsController < ApplicationController
+  def index
+    @artist = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      redirect_to new_song_path
+    end
+
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+
+    redirect_to artist_path(@artist)
+  end
+
+  def destroy
+  end
+
+  def new
+    @artist = Artist.new
+  end
+  
+  private
+  
+    def artist_params
+      params.require(:artist).permit(:name, :bio)
+    end
+end
