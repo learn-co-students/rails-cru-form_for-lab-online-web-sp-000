35288785281968180a9fd47966b48b5085890b29
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-02 21:52:00 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,26 @@
+class ArtistsController < ApplicationController
+
+  def show
+    @artist = Artist.find_by_id(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(name: params[:artist][:name], bio: params[:artist][:name])
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find_by_id(params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by_id(params[:id])
+    @artist.update(name: params[:artist][:name], bio: params[:artist][:name])
+    redirect_to artist_path(@artist)
+  end
+
+end
