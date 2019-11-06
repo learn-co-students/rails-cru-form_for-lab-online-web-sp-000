ec184b6f75c5b61b6f7125760488857b11f55a92
rohanphillips <rohan@solutions-insurance.com>
2019-11-06 14:21:12 -0600

    adding framework

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,13 @@
+class SongsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/new'
+
+  get 'artists/update'
+
+  get 'artists/show'
+
+  get 'genres/index'
+
+  get 'genres/new'
+
+  get 'genres/update'
+
+  get 'genres/show'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
