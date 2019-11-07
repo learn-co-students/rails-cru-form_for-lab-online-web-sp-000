b2f12352b2e55327c961e78b10ab1d0c098ee1ad
rohanphillips <rohan@solutions-insurance.com>
2019-11-07 15:23:33 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,36 @@
+require 'pry'
 class ArtistsController < ApplicationController
   def index
   end
 
   def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
   def update
-  end
+    binding.pry
+    @artist = Artist.find_by(params[:id])
+	  @artist.update(artist_params(:name, :bio))
+	  redirect_to artist_path(@artist)
+	end
 
   def show
+    @artist = Artist.find(params[:id])
   end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+ 
+	def artist_params(*args)
+		params.require(:artist).permit(*args)
+	end
 end
