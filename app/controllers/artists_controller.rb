490fcf69756d55a5babfed04f57ec785a0c8b23e
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-11-08 23:48:22 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,34 @@
+class ArtistsController < ApplicationController
+
+    def index
+     @artists = Arists.all
+    end
+
+    def show
+      @artist = Artist.find(params[:id])
+    end
+  
+    def new
+      @artist = Artist.new
+    end
+  
+    def create
+      @artist = Artist.new(artist_params)
+      @artist.save
+      redirect_to artist_path(@artist)
+    end
+    
+    def update
+      @artist = Artist.find(params[:id])
+      @artist.update(artist_params)
+      redirect_to artist_path(@artist)
+    end
+  
+    private
+  
+    def artist_params(*args)
+        params.require(:artist).permit(*args)
+    end
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+
+  patch 'artists/:id', to: 'artists#update'
+  # resources :artists, except: [:destroy, :index]
+  patch 'genres/:id', to: 'genres#update'
+  # resources :genres, except: [:destroy, :index]
+  patch 'songs/:id', to: 'songs#update'
+  # resources :songs, except: [:detroy, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
