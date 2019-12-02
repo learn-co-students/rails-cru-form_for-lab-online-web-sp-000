38dc5fe31a5826287ba4d539775a448b75e4b9a1
Karen V <karenvo@usc.edu>
2019-12-02 12:37:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'genres/new'
+
+  get 'genres/edit'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/edit'
+
+  resources :genres
+  resources :artists
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
