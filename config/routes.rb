b29acb4932a66cbfd339674766eba3fc6947d5e5
Michael Kofron <kofronmichael@gmail.com>
2019-12-04 22:26:29 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :songs, only: [:new, :show, :create, :edit, :update, :index]
+  resources :genres, only: [:new, :show, :create, :edit, :update]
+  resources :artists, only: [:new, :show, :create, :edit, :update]
 end
