49232f0e1e22eccdbd78b15dac714f90e920a330
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-24 14:38:29 -0500

    All Artist feature spec are passing. Adds show page as well as all controller code

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,18 +1,27 @@
 class ArtistsController < ApplicationController
     layout 'artists_form', only: [:new, :edit]
     def show
+        @artist = Artist.find(params[:id])
     end
    
     def new
+        @artist = Artist.new
     end
 
     def create
+        @artist = Artist.new(post_params)
+        @artist.save
+        redirect_to artist_path(@artist)
     end
 
     def edit
+        @artist = Artist.find(params[:id])
     end
 
     def update
+        @artist = Artist.find(params[:id])
+        @artist.update(post_params)
+        redirect_to artist_path(@artist)
     end
 
     private
