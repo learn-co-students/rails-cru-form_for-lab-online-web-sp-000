ae39627290eb68bfc8f57896266967c88c15c6bd
plabradorjr <plabradorjr@gmail.com>
2020-03-09 10:40:12 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'songs_controller/edit'
+
+  get 'songs_controller/new'
+
+  get 'songs_controller/show'
+
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
