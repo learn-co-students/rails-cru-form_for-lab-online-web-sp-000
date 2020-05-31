f7ff9a51d36e4a478c9d75755b3fd4976ec25e7b
ShaylaWhite <shayladwhite1@gmail.com>
2020-05-31 15:26:07 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,61 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params(:name))
+    @song.artist = Artist.find(params[:song][:artist_id])
+    @song.genre = Genre.find(params[:song][:genre_id])
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+#      def create
+#    	  @song = Song.new(song_params)
+#    	  @song.save
+#    	  redirect_to song_path(@song)
+#    	end
+
+
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name))
+    @song.artist = Artist.find(params[:song][:artist_id])
+    @song.genre = Genre.find(params[:song][:genre_id])
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+#      def update
+#    	  @song = Song.find(params[:id])
+#    	  @song.update(song_params)
+#    	  redirect_to song_path(@song)
+#    	end
+
+
+
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+#      def song_params
+#  			params.require(:song).permit(:name, :artist_id, :genre_id) the **arg are
+#  		end
+end
