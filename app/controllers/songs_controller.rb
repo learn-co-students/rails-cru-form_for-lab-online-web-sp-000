ad2f1ad8010785ad0d2bed3ca168208fbeb9615a
ksummerill <kaitlynsummerill@gmail.com>
2020-06-30 20:38:58 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    song = Song.create(song_params(:name))
+    song = Song.create(song_params(:name, :artist_id, :genre_id))
     song.save
     redirect_to song_path(song)
   end
