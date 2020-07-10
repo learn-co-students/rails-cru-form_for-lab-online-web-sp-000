28fe15ca5d1200beff487eabbd4b357ef0f46b16
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-10 16:26:53 -0700

    update arguments, path names and method names for use with Genre model.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,36 +1,36 @@
 class GenresController < ApplicationController
   
-  def index 
-    @genres = Genre.all 
-  end
+def index
+		@genres = Genre.all
+	end
 
-  def show
-    @genre = Genre.find(params[:id])
-  end
+	def show
+		@genre = Genre.find(params[:id])
+	end
 
-  def new
-    @genre = Genre.new
-  end
+	def new
+		@genre = Genre.new
+	end
 
-  def create 
-    @genre = Genre.new(artist_params)
-    @genre.save
-    redirect_to genre_path(@genre)
-  end
+	def create
+	  @genre = Genre.new(genre_params)
+	  @genre.save
+	  redirect_to genre_path(@genre)
+	end
 
-  def edit 
-    @genre = Genre.find(params[:id])
-  end
+	def edit
+		@genre = Genre.find(params[:id])
+	end
 
-  def update
-    @genre = Genre.find(params[:id])
-    @genre.update(artist_params)
-    redirect_to artist_path(@genre) 
-  end
+	def update
+	  @genre = Genre.find(params[:id])
+	  @genre.update(genre_params)
+	  redirect_to genre_path(@genre)
+	end
 
-  private
+	private
 
-  def artist_params
-    params.require(:genre).permit(:name)
-  end
+		def genre_params
+			params.require(:genre).permit(:name)
+		end
 end
