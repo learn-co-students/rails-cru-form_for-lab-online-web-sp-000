03b8b475089146e4854c76180a6c641024ea8505
Ronda Cowen <cowenhome5@gmail.com>
2020-07-14 21:59:59 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,9 @@ class ArtistsController < ApplicationController
     def index
         @artists = Artist.all
     end
+
     def show
+        binding.pry
         @artists = Artist.find(params[:id])
     end
 
