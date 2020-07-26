9cbeac6253f6eabd3d810cd6609dc5643f2d4e2f
marcon29 <online@markstabler.com>
2020-07-26 11:14:17 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create(name: "Artist1", bio: "Bio for Artist1.")
+artist2 = Artist.create(name: "Artist2", bio: "Bio for Artist2.")
+artist3 = Artist.create(name: "Artist3", bio: "Bio for Artist3.")
+
+genre1 = Genre.create(name: "Genre1")
+genre2 = Genre.create(name: "Genre2")
+
+Song.create(name: "Song1-11", artist_id: artist1.id, genre_id: genre1.id)
+Song.create(name: "Song2-11", artist_id: artist1.id, genre_id: genre1.id)
+Song.create(name: "Song3-11", artist_id: artist1.id, genre_id: genre1.id)
+Song.create(name: "Song4-22", artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name: "Song5-22", artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name: "Song6-22", artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name: "Song7-31", artist_id: artist3.id, genre_id: genre1.id)
+Song.create(name: "Song8-31", artist_id: artist3.id, genre_id: genre1.id)
+Song.create(name: "Song9-32", artist_id: artist3.id, genre_id: genre2.id)
+Song.create(name: "Song10-32", artist_id: artist3.id, genre_id: genre2.id)
+
+
+
