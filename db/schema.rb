eadbd8bc587af2e888878bfc6584aca349b65f22
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-31 19:29:45 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406115425) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
