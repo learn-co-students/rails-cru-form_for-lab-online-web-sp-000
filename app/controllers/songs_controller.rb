a626e470eb13218ec267ab52cde7f43dcba7987b
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-09 06:40:45 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,23 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+
+  private
+
+    def song_params
+      params.require(:song).permit(:name, :artist_id, :genre_id)
+    end
 end
