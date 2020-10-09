fd1351b8adeb7d839d8c0582d2003cb960920688
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-09 07:25:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,6 +18,17 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
   end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+
 
   private
 
