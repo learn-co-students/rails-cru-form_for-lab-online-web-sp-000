0dad7f5751f15bee907fbbaeb69768dbb2a4b8fc
codyalvarez <jedi.alvarez805@gmail.com>
2020-11-12 14:53:42 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,15 +4,28 @@ class ArtistsController < ApplicationController
     @artists = Artist.all 
   end
 
-  def create
-    @artist = Artist.new
+  def show
+    @artist = Artist.find(params[:id])
   end
 
-  def show
+  def new
+    @artist = Artist.new 
+  end
+
+  def create
+    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist.permit(:name, :bio))
+    redirect_to artist_path(@artist)
   end
-
 
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,30 @@
 class GenresController < ApplicationController
  
   def index
-  end
-
-  def create
-  end
-
-  def update
+    @genres = Genre.all 
   end
 
   def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(params.require(:genre).permit(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
   end
 end
