e9924184c05c5ef397ef79a9074ffa85abd6eb9a
TechnicallyAustin <TechnicallyAustin@gmail.com>
2021-03-18 11:48:01 -0700

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,36 @@
 class GenresController < ApplicationController
+    def index
+        @genres = Genre.all
+    end
+
+    def show
+        @genre = genre_finder
+    end
+
+    def create
+        @genre = Genre.new(params(:name))
+    end
+
+    def new
+        @genre = Genre.new
+    end
+
+    def update
+        @genre = Genre.new(params(:name ))
+    end
+
+    def edit
+        @genre = genre_finder
+    end
+
+    private
+
+    def genre_params(*args)
+        params.require(:genre).permit(*args)
+    end
+
+    def song_finder
+        @genre = Genre.find(params[:id])
+    end
+
 end
