73da1ca634e4f20e343948a0cbe47c447c70c149
Christerle Nounoute <christerlesn@gmail.com>
2021-04-23 19:55:41 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,24 @@
 class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find_by(params[:id])
+  end
+
+
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
