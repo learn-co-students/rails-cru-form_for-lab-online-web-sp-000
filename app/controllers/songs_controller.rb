2407124d776afe28078580f1dcd6bad9074196bd
Christerle Nounoute <christerlesn@gmail.com>
2021-04-23 20:37:16 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,6 +32,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:name, :bio)
+    params.require(:song).permit(:name, :artist_id, :genre_id)
   end
 end
